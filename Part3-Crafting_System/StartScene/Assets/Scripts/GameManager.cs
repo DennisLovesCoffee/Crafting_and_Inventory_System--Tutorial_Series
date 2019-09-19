a430fb1c27b8d549daa46e3e4336b59f4035b8ad
Dennis <vymerd@gmail.com>
2019-09-19 08:31:49 +0200

    Crafting system in unity, Part3 of the youtube series

---
+++ Part3-Crafting_System/StartScene/Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<Item> itemList = new List<Item>();
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Inventory.instance.AddItem(itemList[Random.Range(0, itemList.Count)]);
+        }
+    }
+
+}
